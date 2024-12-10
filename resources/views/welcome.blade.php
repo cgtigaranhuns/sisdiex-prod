3a95595994a7fdaefce23c2934cab0e4751f8feb
cgtigaranhuns <cgti@garanhuns.ifpe.edu.br>
2024-12-10 09:35:48 -0300

    home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,99 +19,103 @@
                 <td class="fs-3 text-center align-middle text-secondary">
                     <p>SISDIEX - Sistema da Divisão de Extensão<br>
                         IFPE - Campus Garanhuns</p>
-                    
+
                 </td>
             </tr>
         </table>
     </div>
 
     <div class=" table-responsive container" style="margin-top: -0.5%">
-                    <table class="table table-success height: 100%">
+        <table class="table table-success height: 100%">
 
-                        <tbody>
-                            <tr>
-                                <td style="align-items: flex-end;">
-                               
-                                    <div class="card bg-body-tertiary" style="width:20rem; height: 12rem;  float: right;">
-                                        <!--   <img width="69" height="69" src="img/gestao.png" alt="add-bookmark--v1" /> -->
-                                        <div class="card-body">
-                                            <h5 class="card-title">SISDIEX - Gestão e Propostas</h5>
-                                            <p class="card-text">Gestão e cadastro de Propostas para eventos e ações.</p>
-                                            <br>
-                                            <a href="{{ url('admin/login') }}" class="btn btn-success">Acessar</a>
-                                        </div>
-                                    </div>
-                                 
-                                </td>
-                                <td>
-                                    <div class="card bg-body-tertiary" style="width: 20rem; height: 12rem; float: left;">
-                                        <!--      <img width="69" height="69" src="img/inscricao.png" alt="add-bookmark--v1"> -->
-                                        <div class="card-body">
-                                            <h5 class="card-title">SISDIEX - Inscrições e Certificados.</h5>
-                                            <p class="card-text">Realize inscrições e baixe seus certificados.</p>
-                                            <a href="{{url('https://sisdiex.garanhuns.ifpe.edu.br')}}" class="btn btn-success">Acessar</a>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="card bg-body-tertiary" style="width: 20rem; height: 12rem; float: right; ">
-                                        <!--   <img width="69" height="69" src="img/consulta.png" alt="add-bookmark--v1"> -->
-                                        <div class="card-body">
-                                            <h5 class="card-title">SISDIEX - Validar Certificado de Participação.</h5>
-                                            <p class="card-text">Consulte a validade dos certificados de participação.</p>
-                                            <a href="{{ route('ValidaCertificadoParticipante') }}" class="btn btn-success">Acessar</a>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="card bg-body-tertiary" style="width: 20rem; height: 12rem; float: left;">
-                                        <!--    <img width="69" height="69" src="img/consulta.png" alt="add-bookmark--v1"> -->
-                                        <div class="card-body">
-                                            <h5 class="card-title">SISDIEX - Validar Certificado de Ministrantes</h5>
-                                            <p class="card-text">Consulte a validade dos certificados dos ministrantes.</p>
-                                            <a href="{{ route('ValidaCertificadoMinistrante') }}" class="btn btn-success">Acessar</a>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                    <td colspan="2">
-                        <div class="card bg-body-tertiary" style="width: 41rem; height: 10rem; margin-left: 14rem;">
-                            <!--   <img width="69" height="69" src="img/consulta.png" alt="add-bookmark--v1"> -->
+            <tbody>
+                <tr>
+                    <td style="align-items: flex-end;">
+
+                        <div class="card bg-body-tertiary" style="width:20rem; height: 12rem;  float: right;">
+                            <!--   <img width="69" height="69" src="img/gestao.png" alt="add-bookmark--v1" /> -->
                             <div class="card-body">
-                                <h5 class="card-title" style="color: brown;">SISDIEX - ATÉ 2023 - GERAR E VALIDAR CERTIFICADOS</h5>
-                                <p class="card-text">Gere e consulte os certificados de participação de curso
-                                    ou evento até dezembro de 2023.</p>
-                                <a href="http://200.133.8.49/sisdiex/form_inscricoes/form_inscricoes.php" class="btn btn-success">Acessar</a>
+                                <h5 class="card-title">SISDIEX - Gestão e Propostas</h5>
+                                <p class="card-text">Gestão e cadastro de Propostas para eventos e ações.</p>
+                                <br>
+                                <a href="{{ url('admin/login') }}" class="btn btn-success">Acessar</a>
+                            </div>
+                        </div>
+
+                    </td>
+                    <td>
+                        <div class="card bg-body-tertiary" style="width: 20rem; height: 12rem; float: left;">
+                            <!--      <img width="69" height="69" src="img/inscricao.png" alt="add-bookmark--v1"> -->
+                            <div class="card-body">
+                                <h5 class="card-title">SISDIEX - Inscrições e Certificados.</h5>
+                                <p class="card-text">Realize inscrições e baixe seus certificados.</p>
+                                <a href="{{url('https://sisdiex.garanhuns.ifpe.edu.br')}}" class="btn btn-success">Acessar</a>
                             </div>
                         </div>
                     </td>
-
                 </tr>
+                <tr>
+                    <td>
+                        <div class="card bg-body-tertiary" style="width: 20rem; height: 12rem; float: right; ">
+                            <!--   <img width="69" height="69" src="img/consulta.png" alt="add-bookmark--v1"> -->
+                            <div class="card-body">
+                                <h5 class="card-title">SISDIEX - Validar Certificado de Participação.</h5>
+                                <p class="card-text">Consulte a validade dos certificados de participação.</p>
+                                <a href="{{ route('ValidaCertificadoParticipante') }}" class="btn btn-success">Acessar</a>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="card bg-body-tertiary" style="width: 20rem; height: 12rem; float: left;">
+                            <!--    <img width="69" height="69" src="img/consulta.png" alt="add-bookmark--v1"> -->
+                            <div class="card-body">
+                                <h5 class="card-title">SISDIEX - Validar Certificado de Ministrantes</h5>
+                                <p class="card-text">Consulte a validade dos certificados dos ministrantes.</p>
+                                <a href="{{ route('ValidaCertificadoMinistrante') }}" class="btn btn-success">Acessar</a>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                </tr>
+            </tbody>
+        </table>
+    
+    <table>
+        <tbody>
+            <tr>
+                <td>
+                    <div class="card bg-body-tertiary" style="flex: 1; height: 10rem;">
+                        <div class="card-body">
+                            <h5 class="card-title" style="color: brown;">SISDIEX - ATÉ 2023 - GERAR E VALIDAR CERTIFICADOS</h5>
+                            <p class="card-text">Gere e consulte os certificados de participação de curso ou evento até dezembro de 2023.</p>
+                            <a href="http://200.133.8.49/sisdiex/form_inscricoes/form_inscricoes.php" class="btn btn-success">Acessar</a>
+                        </div>
+                    </div>
+                </td>
 
-                        </tbody>
-                    </table>
+            </tr>
+
+        </tbody>
+    </table>
     </div>
     <div class=" table-responsive container text-end fs-6 opacity-50">
         Desenvolvido pela CGTI - IFPE Campus Garanhuns - 2023 - Versão 1.0
-     <div>
+        <div>
 
-     <div class=" table-responsive container text-center fs-6 opacity-70" style="margin-top: 20px">
-     <p>INSTITUTO FEDERAL DE PERNAMBUCO - IFPE Campus Garanhuns<br>
-     Rua Pe. Agobar Valença, s/n, Severiano de Moraes Filho, Garanhuns - PE CEP: 55299-390.<br> O acesso rodoviário se dá pela via PE 177.</p>
-     
-     <div>
+            <div class=" table-responsive container text-center fs-6 opacity-70" style="margin-top: 20px">
+                <p>INSTITUTO FEDERAL DE PERNAMBUCO - IFPE Campus Garanhuns<br>
+                    Rua Pe. Agobar Valença, s/n, Severiano de Moraes Filho, Garanhuns - PE CEP: 55299-390.<br> O acesso rodoviário se dá pela via PE 177.</p>
 
-
+                <div>
 
 
 
 
 
 
-        </div>
+
+
+                </div>
 
 
 
